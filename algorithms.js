26940a729e886b11345606df711756b26a995cff
ryan <ball.ryan.rb@gmail.com>
2021-09-22 18:10:52 -0600

    adds Grading Students algorithm solution

--- algorithms.js
+++ algorithms.js
@@ -16,19 +16,18 @@ function birthdayCakeCandles(candleNums) {
 
 // console.log(birthdayCakeCandles(candles));
 
-
 // Time Conversion
 // Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
 
 let firstTime = "12:01:00PM";
 let secondTime = "12:01:00AM";
 
-let timeConversion = (time) => {
+const timeConversion = (time) => {
   let hours = time.substr(0, 2);
   let minutes = time.substr(3, 2);
   let seconds = time.substr(6, 2);
   let timeOfDay = time.substr(-2, 2);
-  
+
   if (timeOfDay === "PM" && hours === "12") {
     return `${hours}:${minutes}:${seconds}`;
   } else if (timeOfDay === "PM" && hours !== "12") {
@@ -41,3 +40,24 @@ let timeConversion = (time) => {
 };
 
 console.log(timeConversion(secondTime));
+
+// Grading Students
+// Write code to automate the rounding process for grading of students
+
+const gradingStudents = (grades) => {
+  let roundedGrades = [];
+  grades.forEach((grade) => {
+    if (grade < 38) {
+      roundedGrades.push(grade);
+    } else if (grade % 5 === 3) {
+      roundedGrades.push(grade + 2);
+    } else if (grade % 5 === 4) {
+      roundedGrades.push(grade + 1);
+    } else {
+      roundedGrades.push(grade);
+    }
+  });
+  return roundedGrades;
+};
+
+console.log(gradingStudents([73, 67, 38, 33]));
