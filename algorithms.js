fe2250c5cbe2bae1b9eba5a93923a8c2f1eb5940
ryan <ball.ryan.rb@gmail.com>
2021-12-24 11:33:07 -0700

    adds solution to Divisible Sum Pairs algorithm

--- algorithms.js
+++ algorithms.js
@@ -152,5 +152,16 @@ function birthday(s, d, m) {
 
 // Divisible Sum Pairs
 const divisibleSumPairs = (n, k, ar) => {
-  
+  let count = 0;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+  return count;
 };
+
+divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]);
