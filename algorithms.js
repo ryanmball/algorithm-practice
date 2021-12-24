c575331195509595a2b5629f9cb5f5d06cc99d16
ryan <ball.ryan.rb@gmail.com>
2021-12-23 22:41:00 -0700

    adds solution to Between Two Sets algorithm

--- algorithms.js
+++ algorithms.js
@@ -14,7 +14,6 @@ function birthdayCakeCandles(candleNums) {
 }
 // console.log(birthdayCakeCandles(candles));
 
-
 // Time Conversion
 // Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
 let firstTime = "12:01:00PM";
@@ -38,7 +37,6 @@ const timeConversion = (time) => {
 };
 // console.log(timeConversion(secondTime));
 
-
 // Grading Students
 // Write code to automate the rounding process for grading of students
 const gradingStudents = (grades) => {
@@ -58,7 +56,6 @@ const gradingStudents = (grades) => {
 };
 // console.log(gradingStudents([73, 67, 38, 33]));
 
-
 // Example of using Object.fromEntries functionality
 // var fruits = [
 //   { name: "apple", color: "red" },
@@ -66,7 +63,6 @@ const gradingStudents = (grades) => {
 //   { name: "grape", color: "purple" },
 // ];
 // console.log(Object.fromEntries(fruits.map((key) => [key.name, key.color])));
-
 
 // Count Apples & Oranges
 const appleAndOrange = (s, t, a, b, apples, oranges) => {
@@ -76,7 +72,7 @@ const appleAndOrange = (s, t, a, b, apples, oranges) => {
       count[0]++;
     }
   });
-  oranges.forEach(orange => {
+  oranges.forEach((orange) => {
     if (b + orange >= s && b + orange <= t) {
       count[1]++;
     }
@@ -85,7 +81,6 @@ const appleAndOrange = (s, t, a, b, apples, oranges) => {
   console.log(count[1]);
 };
 // appleAndOrange(2, 3, 1, 5, [2], [-2]);
-
 
 // Number Line Jumps
 const kangaroo = (x1, v1, x2, v2) => {
@@ -100,6 +95,19 @@ const kangaroo = (x1, v1, x2, v2) => {
   }
 };
 
-console.log(kangaroo(0, 3, 4, 2));
+// console.log(kangaroo(0, 3, 4, 2));
 
 // Between Two Sets
+const getTotal = (a, b) => {
+  const isFactor = (a, b) => a % b === 0;
+
+  var results = [];
+  for (var i = a[a.length - 1]; i <= b[0]; i++) {
+    if (a.every((x) => isFactor(i, x)) && b.every((x) => isFactor(x, i))) {
+      results.push(i);
+    }
+  }
+  console.log(results.length);
+};
+
+getTotal([2, 4], [16, 32, 96]);
